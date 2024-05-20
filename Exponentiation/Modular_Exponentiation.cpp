6476267d9b64025e83bcd32ea3dc625527545323
siddhi-bajpai <115869460+siddhi-bajpai@users.noreply.github.com>
2024-05-20 12:41:46 +0530

    Add files via upload

---
+++ Exponentiation/Modular_Exponentiation.cpp
@@ -0,0 +1,43 @@
+//Problem: https://www.geeksforgeeks.org/problems/modular-exponentiation-for-large-numbers5537/1
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	long long int PowMod(long long int x,long long int n,long long int M)
+        {
+            // Code here
+            if(n==0)
+            return 1;
+            if(n==1)
+            return x%M;
+            
+            long long res=PowMod(x,n/2,M);
+            res=(res*res)%M;
+            if(n%2==0)
+            return res;
+            else
+            return (res*x)%M;
+            
+        }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int x, n, m;
+    	cin >> x >> n >> m;
+    	Solution ob;
+    	long long int ans = ob.PowMod(x, n, m);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
